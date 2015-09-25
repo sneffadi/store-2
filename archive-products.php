c2a25c89ed2d8bd245fc22e90ef304b4e5327a02
Stephanie <sneffadi@gmail.com>
2015-09-25 11:39:02 -0600

    first finish

--- archive-products.php
+++ archive-products.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
@@ -18,46 +19,100 @@
 
 get_header(); ?>
 
-<div class="row">
+<div class="row collapse">
 <!-- Row for main content area -->
-	<div class="small-24 medium-16 columns" role="main">
+	<div class="small-24 medium-24 columns" role="main">
 
-	<?php if ( have_posts() ) : ?>
-		<?php 
-		echo "<h4>All Products</h4>";	
-		echo "<div id=\"product_list\">"; ?>
+	<?php
+if (have_posts()): ?>
+		<?php
+    $options = get_option('theme_options');
+    $num = !empty($options['ratings']) ? $options['ratings'] : "100";
+    echo "<h1>All Products</h1>";
+    echo "<div id=\"product_list\">"; ?>
+    	<div class="row collapse">
 		<?php query_posts($query_string . '&orderby=title&order=ASC'); ?>
+		<?php
+			$i = 1;
+	    	while (have_posts()):
+		        the_post();
+		      	global $post;
+		      	$id = $post->ID;
+		      	$image = get_the_post_thumbnail($id, 'medium');
+				$link = get_permalink($id);
+				$end = '';
+				if ($i == sizeof($posts)) { $end = " end"; }
 
-		<?php while ( have_posts() ) : the_post(); ?>
+    	?>
+				<div class="small-24 medium-8 columns<?php echo $end; ?>">
+					<?php
+						if ( $i % 3  == 0) {
+							$colCount = 'three';
+						} elseif ( $i % 3  == 1)  {
+							$colCount = 'one';
+						} elseif ( $i % 3 == 2 ) {
+							$colCount = 'two';
+						}
+					?>
+					<div class="product-item <?php echo $colCount; ?>">
+					<?php
+				        echo "<div class=\"product-name\">";
+				        echo "<a href=\"" . get_permalink($post->ID) . "\">";
+				        echo $post->post_title;
+				        echo "</a>";
+				        echo "</div>";
+				        echo "<figure>";
+				        echo "<a href=\"" . $link . "\">";
+				        echo $image;
+				        echo "</a>";
+				        echo "</figure>";
+					?>
+					<div class="ratings-container">
+						<div class="star-positioner">
+							<div class="stars">
+								<div class="colorbar" style="width:<?php echo get_post_meta($id, 'ratings-overall-value', true) * 20; ?>%">
+								</div> <!-- / .colorbar -->
+								<div class="star_holder">
+									<div class="star star-1"></div> <!-- / .star -->
+									<div class="star star-2"></div> <!-- / .star -->
+									<div class="star star-3"></div> <!-- / .star -->
+									<div class="star star-4"></div> <!-- / .star -->
+									<div class="star star-5"></div> <!-- / .star -->
+								</div> <!-- / .star_holder -->
+							</div> <!-- / .stars -->
+						</div> <!-- / .star-positioner -->
+				        <?php echo "<div class=\"rating\">" . number_format(get_post_meta($id, "ratings-overall-value", true), 1) . "/" . $num . "</div>"; ?>
+			        </div> <!-- / .ratings-container -->
+				    <div class="msrp">
+				        	Retail: $<?php echo (get_post_meta($id, "retail_c1_r1", true)); ?>
+				    </div>
+				    <div class="our-price">
+				        	Our Price: <a href="<?php echo $link; ?>">$<?php echo get_post_meta($id, "price_c1", true); ?></a>
+				    </div>
+				    <div class="row">
+					    <div class="small-24 columns">
+					    	<a href="<?php echo $link; ?>" class="button success tiny radius">Read More</a>
+					    </div> <!-- / .small-12 -->
+				    </div> <!-- / .row -->
+				</div> <!-- / .product-item -->
+			</div><!-- /. small-24 medium-6 columns -->
+		<?php
+		$i++;
+    endwhile; ?>
+    </div> <!-- / .row -->
 
+		<?php
+    echo "</div>"; ?>
+
+		<?php
+else: ?>
 			<?php
-				echo "<a href=\"" . get_permalink($post->ID) . "\">";
-				echo $post->post_title;
-				echo "<figure>";
-                echo "<a href=\"" . get_the_permalink($post->ID) . "\">";
-                echo get_the_post_thumbnail($post->ID);
-                echo "</a>";
-                echo "</figure>";
-				echo "</a>";
-			?>
-
-		<?php endwhile; ?>
-		<?php echo "</div>"; ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-	<?php endif; // End have_posts() check. ?>
-
-	<?php /* Display navigation to next/previous pages when applicable */ ?>
-	<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-		</nav>
-	<?php } ?>
-
+    get_template_part('content', 'none'); ?>
+	<?php
+endif;
+ // End have_posts() check.
+ ?>
 	</div>
-	    <?php get_sidebar(); ?>
-
 </div>
-<?php get_footer(); ?>
+<?php
+get_footer(); ?>
--- modules/google_analytics.php
+++ modules/google_analytics.php
@@ -6,7 +6,7 @@ function add_google_analytics() {
 	$ga .= "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n\t";
 	$ga .= "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n\t";
 	$ga .= "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n\t";
-	$ga .= "ga('create', '" . get_option('theme_options')['gaID'] . "', 'auto');\n\t";
+	$ga .= "ga('create', '" . $options['gaID'] . "', 'auto');\n\t";
 	if (!empty( $add_google_analytics['optimizelyID'] )) {
 		$ga .= "window.optimizely = window.optimizely || [];\n\t";
 		$ga .= "window.optimizely.push(\"activateUniversalAnalytics\");\n\t";
