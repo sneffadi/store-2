4bb91f92b8a59ea505ed881889b74a7dfb3693ec
parkermaughan <parkermaughan@gmail.com>
2015-10-14 12:03:53 -0600

    updated footer

---
+++ privacy.php
@@ -0,0 +1,81 @@
+<script type="text/javascript">
+	function safePop(URL,width,height) {width = width || '475';height = height || '500';day = new Date();id = day.getTime();eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1,width="+width+", height="+height+",left = 0,top = 0');");}
+</script>
+<style type="text/css">
+#content{margin:30px;}
+p{font-family:"Book Antiqua", "Palatino Linotype", Palatino, serif; font-size:14px; line-height:20px; margin:0 0 20px;}
+h1{font-size:14px; font-weight:bold; text-align:center;}
+h2{font-size:14px; font-weight:bold; margin:25px 0 0;}
+h3{font-size:14px; font-style:italic; margin:25px 0 0;}
+</style>
+<div id="content">
+<p><b>Last Updated and Effective: <?php echo date("F d, Y");?></b></p>
+
+<h1>Privacy Policy</h1>
+
+<h2>Introduction</h2> 
+<p>Welcome to the L-Citrullinereviews.org website (the "Website"). This privacy policy is a full disclosure and explanation of practices regarding the collection, use, and disclosure of information transmitted on the Website or through other related services of Procommercellc, LLC ("Company", "we", and "us"). Company is committed to protecting the privacy of Website users, and safeguarding such users' information insofar as possible. The following sets forth our privacy practices and policies.</p>
+
+<h2>Use of This Site by Children</h2>
+<p>Although the Website may be accessed by children under the age of 13, we will not knowingly collect, maintain or use any personal information from such children, and no part of Company's services are directed to persons under the age of 13.  IF YOU ARE UNDER 13 YEARS OF AGE, PLEASE DO NOT USE OR ACCESS THIS COMPANY'S SERVICES AT ANY TIME OR IN ANY MANNER.</p>
+
+<p>If we become aware that we have collected any personal information from children under 13, we will promptly delete such information from our databases.  If you are a parent or guardian and discover that your child under the age of 13 has obtained a Company account, then you may alert Company at <a href="mailto:support@l-citrullinereviews.org">support@l-citrullinereviews.org</a> and request that we delete that child's personal information from our systems.</p>
+
+<h2>Information Collected About You and Our Use of Such Information</h2>
+<p>The following describes the types of personal and other information we may collect about you, and how we use and maintain that information:</p>
+
+<h3>Contact Information</h3>
+<p>In general, you may visit the Website without revealing any personal information other than technology-related information described in the section below.  However, to register with the Website, place an order, or participate in member interaction, you will be required to provide personal information such as, but not limited to, your name, mailing address, e-mail address, date of birth, gender, phone number, billing/credit card information and other information you may provide to us.  We also collect and store any personal information and other data you have provided to us via the Website, by email, or otherwise.</p>
+
+<p>We use this information to respond to any inquiries you may submit to us, to track and monitor usage of the Website and to communicate with you about your account and your use of the Website.  In addition, we may use this information to send you periodic publications associated with Company and/or Company's products, unless you inform us that you opt out from receiving such communications via email. We also use this information to operate the Website; in particular, to facilitate postings on our discussion forums.  We may also use this information for targeted advertising and other marketing purposes. We may also use this information to measure the Website's effectiveness, tailor our products and services, better advertise to suit the personal interests of you and others, estimate our audience size, and any other use that Company believes may enhance your experience using the Website.</p>
+
+<h3>Website Information</h3>
+<p>The Website collects and stores information that is generated automatically as you navigate through the Website.  This aggregate information is collected site-wide, and includes anonymous website statistics. The Website also employs browser cookies to store users' preferences and record session information.  "Cookies" are bits of information that are stored by your browser on your hard drive.  This technology allows for the storage of user preferences, user ID and password, if applicable, and remembers you when you take action on the Website.  You can turn off the "cookie" function through most standard browsers; however, the majority of the features on the Website may not function properly, including but not limited to, the login and order functions, if the ability to accept cookies is disabled.</p>
+
+<p>In addition, when you visit the Website our system automatically collects information such as your web request, Internet Protocol ("IP") address, numbers assigned to your computer by your Internet Service Provider (ISP), browser type, browser language, domain names, referring and exit pages and URLs, platform type, number of clicks, clear gifs, internet tags, landing pages, pages viewed and the order of these page views, the amount of time spent on particular pages, network traffic monitoring, active scripting, the date and time of your request and the unique identity of your browser.  This information is used to analyze trends, administer the Website, improve the design of the Website, track user movement, and otherwise enhance the services we provide.</p>
+
+<h3>Other Information</h3>
+<p>We may retain any data you provide to us in an email or other inquiry, to track the types of questions we receive, analyze trends, administer the Website and services we offer, improve the design of the Website and services we offer, track user movement, and otherwise enhance the services we provide.</p>
+
+<h2>Disclosure and Use of Information</h2>
+<p>We will comply with any subpoenas, court orders or other legal processes for any of the information about you in our possession, and we may use such information to establish or exercise our legal rights, or to defend against legal claims.  We reserve the right to disclose your personal information, including data on which pages you have visited, to third parties if we feel it is necessary to protect our systems, Website, business, users or others.</p>
+
+<p>In addition, information about you in our possession may be disclosed as part of any merger, acquisition, sale of company assets, or transition of service to another provider, as well as in the unlikely event of insolvency, bankruptcy, or receivership in which your personal information would be transferred as one of the business assets of the company. We do not guarantee that any entity receiving such information in connection with one of these transactions will comply with all terms of this policy nor shall Company be liable for any security breaches that may occur after the transfer of information.</p>
+
+<p>We use your personal information to process orders, maintain your membership, store information, and provide marketing or promotional materials.  We may also disclose your personal information to authorized contractors used by Company to support our business and provision of services to you (such as, but not limited to, fulfillment services, customer service, technical support, delivery services, and financial institutions). We will request that such third parties agree to treat your personal information in keeping with the guidelines set forth within this Policy.  We may, at our discretion, disclose personal information, when deemed necessary or appropriate, to law enforcement or authorized government organizations, to protect our rights or the rights of others, to prevent identity theft and harm to persons or property, to fight fraud, to ensure the integrity of our business, or to otherwise protect the right, property, or safety of Company and our employees or others.</p>
+
+<p>In addition, we may make full use of all information acquired through the Website that is not in personally identifiable form, as indicated above. This includes the disclosure of non-identifying transaction information to third parties for the purpose of accounting and record keeping. Disclosure of non-identifying information such as aggregate user data and other similar data may also be provided to prospective business partners, advertisers, and others.</p>
+
+<h2>Opt-Out Procedures</h2>
+<p>Once you register with Company, we may send you administrative-related emails, as well as emails to facilitate your access to Company's services, including providing you with related updates.  We may not offer you the opportunity to opt out of receiving these communications.  If you want to stop receiving these communications, contact use via email at <a href="mailto:support@l-citrullinereviews.org">support@jl-citrullinereviews.org</a>.</p>
+
+<p>However, we provide you with the opportunity to elect not to receive other types of communications, including emails informing you about Company's services and other offerings that we think will interest you.  If you want to stop receiving these communications, click on the "Unsubscribe" link in any such email, or contact use via email at <a href="mailto:support@l-citrullinereviews.org">support@l-citrullinereviews.org</a>.</p>
+
+<p>Be advised that we may respond with an automated message, explaining the steps required to opt-out. There may be a time delay between initializing the process of opting out and the time at which communication is discontinued. During this intervening time, you may continue to receive marketing message or confirmation emails from us. Company will not be liable for any problems, concerns, delays, or damages associated with the opt-out process.</p>
+
+<h2>Links and Third-Party Tools or Services</h2>
+<p>The Website may include access, links, or connection to third party websites. This privacy policy does not apply to these linked websites. Company is not responsible for the privacy practices or the content on any of these other websites, including any websites that may be in a special relationship, business relationship or partnership with Company. The linked sites may collect personal information from you when you access their site. Each website should be checked for its own privacy policy.</p>
+
+<h2>Security of Information</h2>
+<p>Whenever we handle information that you disclose on the Website, regardless of where this occurs, we take steps to ensure that your information is treated securely and in accordance with this privacy policy.  Company uses commercially reasonable physical, managerial and technical safeguards to preserve the integrity and security of your personal information.  Certain information transmitted through the Website, such as Cookies, login information and information relating to the purchase of our services, is encrypted using Secure Sockets Layer (SSL) technology or similar encryption technology, which provides for secure online ordering and payment processing.  Unfortunately, we cannot ensure or warrant the security of any information you transmit to us, and you do so at your own risk.  Once we receive your transmission, we make our best effort to ensure its security on our systems but you acknowledge that (a) there are security and privacy limitations which are beyond our control, (b) the security, integrity, and privacy of any and all information, and the data exchanged between you and us through the Website cannot be guaranteed, and (c) any information and data may be viewed or tampered with in transit by a third party, without our knowledge and without our ability to prevent such tampering. You choose to transmit information on the Website at your own risk. Furthermore, Company will not be liable for any problems that may result from the breach of security resultant from the transmission of your personal information or any unintentional disclosure.</p>
+
+<h2>Community Interaction, Public Comments, Chat Rooms and Message Boards</h2>
+<p>The Website may include online forums, chat rooms, message boards, and other interactive technologies that allow comments, messages, emails, and postings to be associated with a user's name or email address. Be aware that any information you post is public information, and Company does not control what use third parties may make of this personal information. Your participation in such forums is at your own risk. Company will not be held responsible for any breach of privacy that occurs due to your involvement in such forums.</p>
+
+<h2>California Privacy Notification</h2>
+<p><a href="http://www.leginfo.ca.gov/cgi-bin/displaycode?section=civ&group=01001-02000&file=1798.80-1798.84">California Civil Code Section 1798.83</a> allows customers who are California residents to request and obtain once a year, information about the personal information (if any) disclosed to third parties for direct marketing purposes in the preceding calendar year. This information is provided by Company free of charge. If applicable, this information would include a list of the categories of personal information that was shared, and the names and addresses of all third parties with which the information was shared in the preceding twelve calendar months. If you are a California resident and would like to request this information please submit your request in writing by electronic mail to <a href="mailto:support@l-citrullinereviews.org">support@l-citrullinereviews.org</a> or in writing to:</p>
+
+<p>Attn: California Privacy Rights<br>
+4778 North 300 West<br>
+Suite 120<br>
+Provo, UT 84604</p>
+
+<h2>Changes to Policy</h2>
+<p>This Policy may be changed at any time and for any reason, and Company reserves the right to make such changes at its sole discretion. Your continued use of the Website after the changes are made and posted on the Website constitutes your agreement to these changes, both with regard to information previously collected from you and with regard to information collected in the future. If you do not agree to the changes, please discontinue your use of the Website.</p>
+
+<h2>GOVERNING LAW</h2>
+<p>The Website is controlled by Company from our offices within the State of Utah, United States of America.  By choosing to visit the Website or otherwise provide information to Company, you agree that any dispute over privacy or the terms contained in this privacy policy will be governed by the law of the State of Utah.  If you are accessing the Website from any region with regulations or laws governing personal data collection, use or disclosure that differ from United States laws or regulations, please note that through your continued use of the Website, which is governed by the laws of the State of Utah and the United States of America, this privacy policy and our Terms of Use (available  <a href="javascript:safePop('http://www.l-citrullinereviews.org/terms.php')">here</a> and/or at <a href="http://www.l-citrullinereviews.org/">http://www.l-citrullinereviews.org</a>), you are transferring personal information to the United States of America and you consent to that transfer.  You also consent to the adjudication of any disputes arising in connection with the Website in the federal and state courts of Utah County in the State of Utah.  You also agree to arbitrate such disputes and to abide by any limitation on damages contained in our Terms of Use or other agreement we have with you.</p>
+
+<h2>UPDATE INFORMATION / SUGGESTIONS</h2>
+<p>If you would like to update or correct any information that you have provided to Company through your use of the Website or otherwise, or you have suggestions for improving this Policy, please send an email to <a href="mailto:support@l-citrullinereviews.org">support@l-citrullinereviews.org</a>.</p>
+</div>
